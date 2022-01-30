fa7c6d44380470d65df90caa6374ee630913830c
DarkSonic <dineshkumar_b@hotmail.com>
2022-01-31 00:55:07 +0530

    first commit

---
+++ polls/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from django.conf.urls import url
+
+from . import views
+
+
+def index(request):
+    
+    return render(request, 'polls/index.html')
